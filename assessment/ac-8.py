d6728d355a731739e693b04a80f4628c5eb3af68
Daniel Compton <daniel.compton@nist.gov>
2022-11-10 20:50:08 -0600

    Adding assessment should handle the result part when working.

---
+++ assessment/ac-8.py
@@ -0,0 +1,10 @@
+### This is the script containing tests for security assessment.
+#
+# This would be the tests written by developers and/or security staff to perform testing
+# on the application based on the organizational security requirements.
+#
+# This script could exist in parts to test certain controls, or it could reside in a 
+# completed different repository and referenced as a submodule.
+
+print("CONTROL AC-8 TESTED: PASS [This message originates from script/app_assessment.py]")
+assert True == True
