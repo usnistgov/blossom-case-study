0cab3b7ff94e79c4351a1d6e2374229dd01e5f08
Daniel Compton <daniel.compton@nist.gov>
2022-11-10 20:37:50 -0600

    Add validation based on AJs example. (May not work)

---
+++ .github/actions/oscal-validation/docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -l
+
+# chmod +x docker-entrypoint.sh
+
+models=$1
+echo "::set-output name=model::$model"
+
+time=$(date)
+echo "::set-output name=time::$time"
+
+location=$(pwd)
+echo "::set-output name=location::$location"
+
+cat $models
+
+# /bin/bash ghcr.io/usnistgov/blossom/oscal-cli:0.1.0 -c "/opt/oscal-cli/bin/oscal-cli profile resolve --as=json --to=json --quiet /app/docs/rmf_lifecycle/system-security-plans/nist-moderate-example-profile.oscal.json > /app/docs/rmf_lifecycle/catalogs/nist-moderate-example-resolved_catalog.oscal.json 2>/dev/null"
+
+# /opt/oscal-cli/bin/oscal-cli profile resolve --as=json --to=json --quiet /app/*/rmf_lifecycle/system-security-plans/nist-moderate-example-profile.oscal.json"
+ls -ltra
+
+echo "Validating SSPs"
+find ./system-security-plan -type f -name '*.json' -exec /opt/oscal-cli/bin/oscal-cli ssp validate {} \;
